5f73777a71759243422d8f9af23fd3a492d999cd
adil-bourichi <adil.bourichi@laplateforme.io>
2023-05-23 12:29:42 +0200

    job4

---
+++ jour2/job4/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FizzBuzz</title>
+</head>
+<body>
+    <?php
+    for ($i = 1; $i <= 100; $i++) {
+        if ($i % 3 === 0 && $i % 5 === 0) {
+            echo "FizzBuzz";
+        } elseif ($i % 3 === 0) {
+            echo "Fizz";
+        } elseif ($i % 5 === 0) {
+            echo "Buzz";
+        } else {
+            echo $i;
+        }
+
+        echo "<br />";
+    }
+    ?>
+</body>
+</html>
