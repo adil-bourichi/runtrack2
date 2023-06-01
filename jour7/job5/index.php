b5c9476eacce196ffd479a7dd20bda633709c23a
adil-bourichi <adil.bourichi@laplateforme.io>
2023-06-01 12:08:53 +0200

    job5

---
+++ jour7/job5/index.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['grille'])) {
+    $_SESSION['grille'] = [
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']
+    ];
+}
+
+if (isset($_POST['reinitialiser'])) {
+    $_SESSION['grille'] = [
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']
+    ];
+}
+
+function afficherGrille()
+{
+    echo '<table>';
+    for ($i = 0; $i < 3; $i++) {
+        echo '<tr>';
+        for ($j = 0; $j < 3; $j++) {
+            echo '<td>';
+            echo '<form method="post" action="">';
+            echo '<input type="submit" name="case" value="' . $_SESSION['grille'][$i][$j] . '" ';
+            if ($_SESSION['grille'][$i][$j] != '-') {
+                echo 'disabled ';
+            }
+            echo '>';
+            echo '<input type="hidden" name="row" value="' . $i . '">';
+            echo '<input type="hidden" name="col" value="' . $j . '">';
+            echo '</form>';
+            echo '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+
+function verifierVictoire()
+{
+    $grille = $_SESSION['grille'];
+
+    // Vérification des lignes
+    for ($i = 0; $i < 3; $i++) {
+        if ($grille[$i][0] != '-' && $grille[$i][0] == $grille[$i][1] && $grille[$i][0] == $grille[$i][2]) {
+            return true;
+        }
+    }
+
+    // Vérification des colonnes
+    for ($j = 0; $j < 3; $j++) {
+        if ($grille[0][$j] != '-' && $grille[0][$j] == $grille[1][$j] && $grille[0][$j] == $grille[2][$j]) {
+            return true;
+        }
+    }
+
+    // Vérification des diagonales
+    if ($grille[0][0] != '-' && $grille[0][0] == $grille[1][1] && $grille[0][0] == $grille[2][2]) {
+        return true;
+    }
+
+    if ($grille[0][2] != '-' && $grille[0][2] == $grille[1][1] && $grille[0][2] == $grille[2][0]) {
+        return true;
+    }
+
+    return false;
+}
+
+if (isset($_POST['case'])) {
+    $row = $_POST['row'];
+    $col = $_POST['col'];
+    $joueur = ($_SESSION['grille'][$row][$col] == 'X') ? 'O' : 'X';
+    $_SESSION['grille'][$row][$col] = $joueur;
+
+    if (verifierVictoire()) {
+        echo $joueur . ' a gagné !';
+        $_SESSION['grille'] = [
+            ['-', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ];
+    } else {
+        $casesRemplies = true;
+        foreach ($_SESSION['grille'] as $ligne) {
+            if (in_array('-', $ligne
