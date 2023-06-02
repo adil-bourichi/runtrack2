55f61aa6b71688fa86f476bee2ceeceff95940f7
adil-bourichi <adil.bourichi@laplateforme.io>
2023-06-02 11:52:15 +0200

    job3

---
+++ jour9/job3/job3.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+
+    $bdd = new PDO('mysql:host=localhost;dbname=jour09', "root", "Zen4321@");
+
+    $requete = $bdd->prepare("SELECT `prenom`, `nom`, `naissance`FROM `etudiants` WHERE sexe= 'Femme'");
+    $requete->execute();
+    $users = $requete->fetchAll();
+
+    echo "<table border='1'>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th>Prénom</th>";
+    echo "<th>Nom</th>";
+    echo "<th>Naissance</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    foreach($users as $key => $user){
+        echo '<tr>';
+        echo '<td>' . $user['prenom'] . '</td>';
+        echo '<td>' . $user['nom'] . '</td>';
+        echo '<td>' . $user['naissance'] . '</td>';
+        echo '</tr>';
+    }
+    
+    echo '</tbody>';
+    echo '</table>';
+    ?>
+</body>
+
+</html>
