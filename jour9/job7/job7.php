d75c98f25b089471814d46f4405d35ca8afeaeb3
adil-bourichi <adil.bourichi@laplateforme.io>
2023-06-02 12:20:51 +0200

    job7

---
+++ jour9/job7/job7.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+
+    $bdd = new PDO('mysql:host=localhost;dbname=jour09', "root", "Zen4321@");
+
+    $requete = $bdd->prepare("SELECT SUM(superficie) AS total_sup FROM etage");
+    $requete->execute();
+    $users = $requete->fetchAll();
+
+    echo "<table border='1'>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th>total_sup</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    foreach($users as $key => $user){
+        echo '<tr>';
+        echo '<td>' . $user['total_sup'] . '</td>';
+        echo '</tr>';
+    
+    }
+    
+    echo '</tbody>';
+    echo '</table>';
+    ?>
+</body>
+
+</html>
