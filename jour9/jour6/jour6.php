0c7e83f756b448ff964aca823668f278cec59202
adil-bourichi <adil.bourichi@laplateforme.io>
2023-06-02 12:20:27 +0200

    job6

--- jour9/jour6/jour6.php
+++ jour9/jour6/jour6.php
@@ -14,21 +14,21 @@
 
     $bdd = new PDO('mysql:host=localhost;dbname=jour09', "root", "Zen4321@");
 
-    $requete = $bdd->prepare("SELECT SUM(superficie) AS total_sup FROM etage");
+    $requete = $bdd->prepare("SELECT COUNT(*) AS nb_etudiants FROM etudiants");
     $requete->execute();
     $users = $requete->fetchAll();
 
     echo "<table border='1'>";
     echo "<thead>";
     echo "<tr>";
-    echo "<th>total_sup</th>";
+    echo "<th>nb_etudiants</th>";
     echo "</tr>";
     echo "</thead>";
     echo "<tbody>";
 
     foreach($users as $key => $user){
         echo '<tr>';
-        echo '<td>' . $user['total_sup'] . '</td>';
+        echo '<td>' . $user['nb_etudiants'] . '</td>';
         echo '</tr>';
     
     }
